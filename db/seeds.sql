7d0caca9c6017d169d057ea02112ab5bf953498d
Matt Auretta <mauretta2@gmail.com>
2018-10-24 13:25:36 -0600

    functional mysql and created orm

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Black Bean Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Quinoa Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Portabella Burger", false);
